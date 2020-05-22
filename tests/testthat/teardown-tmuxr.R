bca53984c6cdbbcf9161560d39ab341eaf9569a7
Jeroen Janssens <jeroen@datascienceworkshops.com>
2020-05-22 10:40:02 +0000

    version 0.2.4

--- tests/testthat/teardown-tmuxr.R
+++ tests/testthat/teardown-tmuxr.R
@@ -1,3 +1,14 @@
 if (is_installed()) {
+  new_session()
+  Sys.sleep(0.1)
+  socket_path <- getOption("tmux_socket_path")
   kill_server()
+  Sys.sleep(0.1)
+  fs::file_delete(socket_path)
+  Sys.sleep(0.1)
+  if (fs::file_exists(socket_path)) {
+    message("Socket ", socket_path, " could not be deleted")
+  } else {
+    message("Socket ", socket_path, " successfully deleted")
+  }
 }
